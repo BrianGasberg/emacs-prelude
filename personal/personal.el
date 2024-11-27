ce5715e1c66031be0ad5b04a535c7c07da500ec7
Brian Gasberg Thomsen <mail@briangasberg.dk>
2024-11-27 20:27:36 +0100

    stuff

---
+++ personal/personal.el
@@ -0,0 +1,8 @@
+(setq yas-snippet-dirs
+      '("~/.emacs.d/personal/snippets"                 ;; personal snippets
+        "~/.emacs.d/elpa/yasnippet-snippets-20241014.949/snippets" ;; foo-mode and bar-mode snippet collection
+        ))
+
+(yas-global-mode 1) ;; or M-x yas-reload-all if you've started YASnippet already.
+
+(load-theme 'spacemacs-dark)
